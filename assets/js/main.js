1a28882385807aedd2aff3396cf0e1695ae77f76
Losto <92962731+Lostovayne@users.noreply.github.com>
2023-04-14 17:58:38 -0400

    Fixed finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 //selectores
-
 const containerRows = document.querySelector('.containerRows')
 const datosUsuario = document.querySelector('.datosUsuario')
+const NotaNecesaria = document.querySelector('.NotaNecesaria')
 
 //inicializando variables
 let nombre
@@ -9,16 +9,8 @@ let carrera
 let ramo1
 let ramo2
 let ramo3
-// Notas del usuario
-let nota1 = ''
-let nota2 = ''
-let nota3 = ''
-let nota4 = ''
-let nota5 = ''
-let nota6 = ''
-let nota7 = ''
-let nota8 = ''
-let nota9 = ''
+let PIncompleto
+let contador = 0
 
 // Se ejecuta toda la logica despues de cargado el html
 window.addEventListener('DOMContentLoaded', () => {
@@ -37,7 +29,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	//Comprueba los datos del usuario y agrega los datos de forma correspondiente, nombre, carrera y ramo ,llamando a la funcion para agregar las notas
-
 	if (edad >= 18) {
 		nombre = prompt('Ingresa tu nombre y apellidos!')
 		carrera = prompt('Ingresa el nombre de tu carrera')
@@ -47,85 +38,64 @@ window.addEventListener('DOMContentLoaded', () => {
 		RecibirDatos(ramo2)
 		ramo3 = prompt('Ingresa el nombre de tu ramo 3')
 		RecibirDatos(ramo3)
+		DatosPersonas(nombre, carrera)
 	}
-
-	//Se manejará la logica aqui una vez recibidos los datos desde el usuario , creandose la tabla en el html <Main></Main>
-	DatosPersonas(nombre, carrera)
-	CrearHtml(ramo1, nota1, nota2, nota3)
-	CrearHtml(ramo2, nota4, nota5, nota6)
-	CrearHtml(ramo3, nota7, nota8, nota9)
 })
 
-//Funcion pára recibir los datos y generar una especie de bucle solcitando las notas y agregandolas correctamente!
-
+//Funcion para recibir los datos y generar una especie de bucle solcitando las notas y agregandolas correctamente mas la  logica de validacion!
 function RecibirDatos(NombreRamo) {
-	let not1 = prompt(`Ingresa la nota 1 del ramo [${NombreRamo}]`)
+	let not1 = ' '
+	let not2 = ' '
+	let not3 = ' '
+	contador++
+
+	not1 = parseInt(prompt(`Ingresa la nota 1 del ramo [${NombreRamo}]`))
 	//El valor debe ser un numero y no un texto
-	if (isNaN(not1)) {
-		alert('El valor introducido no es un numero , ingresa un numero correctamente!!')
-		not1 = prompt(`Ingresa la nota 1 del ramo [${NombreRamo}]`)
+	if (isNaN(not1) || not1 === '') {
+		alert('El valor introducido no es un numero o se encuentra vacio, agrega un numero correctamente!!')
+		not1 = parseInt(prompt(`Ingresa la nota 1 del ramo [${NombreRamo}]`))
 	}
 
 	// No puede agregar notas mayores a 7 por lo menos en Chile no existem
-	if (Number(not1) > 7) {
+	if (not1 > 7) {
 		alert('No existe una nota mayor a 7 en el sistema de evaluacion nacional!')
-		not1 = prompt(`Ingresa la nota 1 del ramo [${NombreRamo}]`)
+		not1 = parseInt(prompt(`Ingresa la nota 1 del ramo [${NombreRamo}]`))
 	}
-	// ----------------------------------------------
 
 	//Manejo de la nota 2 comprobando que sea un numero antes de agregarla
-	let not2 = prompt(`Ingresa la nota 2 del ramo [${NombreRamo}]`)
-	if (isNaN(not2)) {
-		alert('El valor introducido no es un numero , ingresa un numero correctamente!!')
-		not2 = prompt(`Ingresa la nota 2 del ramo [${NombreRamo}]`)
+	not2 = parseInt(prompt(`Ingresa la nota 2 del ramo [${NombreRamo}]`))
+	if (isNaN(not2) || not2 === '') {
+		alert('El valor introducido no es un numero, o se encuentra vacio, agrega un numero correctamente!!')
+		not2 = parseInt(prompt(`Ingresa la nota 2 del ramo [${NombreRamo}]`))
 	}
 
-	if (Number(not2) > 7) {
+	if (not2 > 7) {
 		alert('No existe una nota mayor a 7 en el sistema de evaluacion nacional!')
-		not2 = prompt(`Ingresa la nota 2 del ramo [${NombreRamo}]`)
+		not2 = parseInt(prompt(`Ingresa la nota 2 del ramo [${NombreRamo}]`))
 	}
-	// ----------------------------------------------
 
 	//Manejo de los datos nota 3
-	let not3 = prompt(`Ingresa la nota 3 del ramo [${NombreRamo}]`)
-	if (isNaN(not3)) {
-		alert('El valor introducido no es un numero , ingresa un numero correctamente!!')
-		not3 = prompt(`Ingresa la nota 3 del ramo [${NombreRamo}]`)
+	if (contador <= 2) {
+		not3 = parseInt(prompt(`Ingresa la nota 3 del ramo [${NombreRamo}]`))
+		if (isNaN(not3) || not3 === '') {
+			alert(
+				'El valor introducido no es un numero, o se encuentra vacio, agrega un numero correctamente!!'
+			)
+			not3 = parseInt(prompt(`Ingresa la nota 3 del ramo [${NombreRamo}]`))
+		}
+
+		if (not3 > 7) {
+			alert('No existe una nota mayor a 7 en el sistema de evaluacion nacional!')
+			not3 = parseInt(prompt(`Ingresa la nota 3 del ramo [${NombreRamo}]`))
+		}
+	} else {
+		not3 = '-'
 	}
 
-	if (Number(not3) > 7) {
-		alert('No existe una nota mayor a 7 en el sistema de evaluacion nacional!')
-		not3 = prompt(`Ingresa la nota 3 del ramo [${NombreRamo}]`)
-	}
-
-	//Comprueba que notas estan vacias para asignarle las notas correspondientes!
-	if (nota1 === '') {
-		nota1 = parseInt(not1)
-		nota2 = parseInt(not2)
-		nota3 = parseInt(not3)
-		alert(` Las notas fueron agregadas correctamente al ramo [${NombreRamo}]`)
-		return
-	}
-
-	if (nota4 === '') {
-		nota4 = parseInt(not1)
-		nota5 = parseInt(not2)
-		nota6 = parseInt(not3)
-		alert(` Las notas fueron agregadas correctamente al ramo [${NombreRamo}]`)
-		return
-	}
-
-	if (nota7 === '') {
-		nota7 = parseInt(not1)
-		nota8 = parseInt(not2)
-		nota9 = '-'
-		alert(` Las notas fueron agregadas correctamente al ramo [${NombreRamo}]`)
-		return
-	}
+	CrearHtml(NombreRamo, not1, not2, not3)
 }
 
-// Crea el Html necesario paramostrar los datos del usuario de manera dinamica!
-
+// Crea el Html necesario para mostrar los datos del usuario de manera dinamica!
 function DatosPersonas(nombre, carrera) {
 	datosUsuario.innerHTML = `	
 	<p>Nombre: ${nombre}</p>
@@ -137,7 +107,9 @@ function DatosPersonas(nombre, carrera) {
 function CrearHtml(ramo, Pnota1, Pnota2, Pnota3) {
 	let promedio = ((Pnota1 + Pnota2 + Pnota3) / 3).toFixed(1)
 	if (isNaN(promedio)) {
+		PIncompleto = Pnota1 + Pnota2
 		promedio = '-'
+		CalcularNotaFaltante(ramo, PIncompleto)
 	}
 
 	const tr = document.createElement('tr')
@@ -151,4 +123,12 @@ function CrearHtml(ramo, Pnota1, Pnota2, Pnota3) {
 	containerRows.appendChild(tr)
 }
 
-// FUNCIONES CALCULO DE NOTAS
+// Calcula la nota faltante para aprobar
+function CalcularNotaFaltante(ramo, Pincompleto) {
+	let resultado = 4 * 3 - Pincompleto
+	if (resultado <= 0) {
+		resultado = 1
+	}
+
+	NotaNecesaria.innerHTML = `Necesitas conseguir un : ${resultado} Para aprobar el ramo de  ${ramo} `
+}
